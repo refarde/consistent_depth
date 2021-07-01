05f11d90f02a0a30e13dc384e75ffdf4a14d0aa2
Wonseop Kim <refarde@gmail.com>
2021-07-02 00:03:57 +0900

    Recovery torch version

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 # conda create -n consistent_depth python=3.6
 # conda activate consistent_depth
 # Note to set LD_LIBRARY_PATH if conda activate fails to do so.
-pip3 install torch==1.9.0+cu102 torchvision==0.10.0+cu102 torchaudio===0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install -r requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
 pushd third_party/flownet2
 chmod +x install.sh
 ./install.sh
